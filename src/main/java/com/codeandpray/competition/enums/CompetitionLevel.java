eb44e02f04ab159c3b6718f235c5d661e45d732f
samurai3452345 <magomeggaziev4@gmail.com>
2026-09-24 19:27:17 +0300

    feat:  create enums; CompetitionFormat, CompetitionLevel, CompetitionStatus

---
+++ src/main/java/com/codeandpray/competition/enums/CompetitionLevel.java
@@ -0,0 +1,9 @@
+package com.codeandpray.competition.enums;
+
+public enum CompetitionLevel {
+    RUSSIAN_CHAMPIONSHIP,
+    NATIONAL,
+    INTERREGIONAL,
+    DAGESTAN_CHAMPIONSHIP,
+    REGIONAL
+}
