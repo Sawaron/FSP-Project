b5e8cc96c037ae41c1a405aceb230e6595ba937e
samurai3452345 <magomeggaziev4@gmail.com>
2026-09-25 21:51:37 +0300

    fix: final fix

--- src/main/java/com/codeandpray/auth/dto/AuthResponse.java
+++ src/main/java/com/codeandpray/auth/dto/AuthResponse.java
@@ -1,15 +1,13 @@
 package com.codeandpray.auth.dto;
 
 import com.codeandpray.auth.enums.UserRole;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
 
-
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public record AuthResponse(String token, Long userId, String email, UserRole role) {
+public record AuthResponse(
+        String token,
+        Long userId,
+        String email,
+        UserRole role
+) {
     @Override
     public String toString() {
         return "AuthResponse[redacted]";
