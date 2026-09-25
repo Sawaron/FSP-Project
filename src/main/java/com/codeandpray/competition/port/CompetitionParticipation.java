1ca32c995149b0d75e30342b114ce980e375ecd5
samurai3452345 <magomeggaziev4@gmail.com>
2026-09-25 11:19:49 +0300

    feat: add CompetitionParticipation and DisciplineDirectory

---
+++ src/main/java/com/codeandpray/competition/port/CompetitionParticipation.java
@@ -0,0 +1,5 @@
+package com.codeandpray.competition.port;
+
+public interface CompetitionParticipation {
+    boolean hasAnyRegistration(long competitionId);
+}
---
+++ src/main/java/com/codeandpray/competition/port/DisciplineDirectory.java
@@ -0,0 +1,5 @@
+package com.codeandpray.competition.port;
+
+public interface DisciplineDirectory {
+    boolean exists(long disciplineId);
+}
