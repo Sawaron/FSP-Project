76a72adb462f019045264a5fd8775e55071081fd
Sawaron <shoma111125@gmail.com>
2026-09-25 22:00:24 +0300

    fixing project

--- src/main/java/com/codeandpray/athlete/controller/AthleteProfileController.java
+++ src/main/java/com/codeandpray/athlete/controller/AthleteProfileController.java
@@ -7,14 +7,8 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/athletes")
